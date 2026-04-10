d1afde9790c89fd5d5c646f7dca815c11a87d096
bobbym.moneyjr <bmmoney261@gmail.com>
2026-04-10 18:52:47 -0400

    Added useState for data types needed to keep track of.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+ 
+ const [username, setUsername] = useState('')
+const [userData, setUserData] = useState(null)
+const [repos, setRepos] = useState([])
+const [loading, setLoading] = useState(false)
+const [errorMessage, setErrorMessage] = useState(null)
+ const [roast, setRoast] = useState(null)
   return (
     <div>
       <h1>GitGraded</h1>
